863549003e9f0b6c363eb9c878bee8dd56ee1ebc
Avishek <73749526+avishekroy87@users.noreply.github.com>
2024-03-16 15:28:55 +0530

    initcomit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+import './App.css'
+
+function App() {
+  const size = screen.width;
+
+  useEffect(()=>{
+    
+  },[])
+
+
+  return (
+    <div className="font-light">The screen size is {size}</div>
+  )
+}
+
+export default App
